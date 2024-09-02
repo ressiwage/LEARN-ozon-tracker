b1cdda1156e83db13747acb7a09827158c9d2e68
rswg <ressiwage@ya.ru>
2024-09-02 16:04:26 +0500

    куки всё-таки нужны были

--- main.py
+++ main.py
@@ -16,7 +16,19 @@ class Config:
     chat_id=None
 conf = Config()
 
-
+cookies = {
+    '__Secure-ETC': 'c1a04ce4fb078e3735a2dbb127dd7503',
+    '__Secure-access-token': '5.0.uBbLfQuQTd-8VErND_LJTg.93.AWnnxzRYVO0ogM0NJMeS1E7yAX7hwkHFURgccy716UEb38LoTvRraeAVw8ccqhRRYg..20240902102319.hucupJP9ZKsh1qtdug52_BKhXaS9w9rYf_HjkvkfN9M.134ac26072ffece95',
+    '__Secure-refresh-token': '5.0.uBbLfQuQTd-8VErND_LJTg.93.AWnnxzRYVO0ogM0NJMeS1E7yAX7hwkHFURgccy716UEb38LoTvRraeAVw8ccqhRRYg..20240902102319.A0ruyNtUsI2UdA362fUzfmUA8HcVFcLv6R-rySKrXjk.17198f35041ebeb10',
+    '__Secure-ab-group': '93',
+    '__Secure-user-id': '0',
+    'xcid': 'f0f481276fc36e9eeffd3d2e79c81dfc',
+    '__Secure-ext_xcid': 'f0f481276fc36e9eeffd3d2e79c81dfc',
+    'ADDRESSBOOKBAR_WEB_CLARIFICATION': '1725265400',
+    'rfuid': 'NjkyNDcyNDUyLDEyNC4wNDM0NzUyNzUxNjA3NCwxMDI4MjM3MjIzLC0xLC05ODc0NjQ3MjQsVzNzaWJtRnRaU0k2SWxCRVJpQldhV1YzWlhJaUxDSmtaWE5qY21sd2RHbHZiaUk2SWxCdmNuUmhZbXhsSUVSdlkzVnRaVzUwSUVadmNtMWhkQ0lzSW0xcGJXVlVlWEJsY3lJNlczc2lkSGx3WlNJNkltRndjR3hwWTJGMGFXOXVMM0JrWmlJc0luTjFabVpwZUdWeklqb2ljR1JtSW4wc2V5SjBlWEJsSWpvaWRHVjRkQzl3WkdZaUxDSnpkV1ptYVhobGN5STZJbkJrWmlKOVhYMHNleUp1WVcxbElqb2lRMmh5YjIxbElGQkVSaUJXYVdWM1pYSWlMQ0prWlhOamNtbHdkR2x2YmlJNklsQnZjblJoWW14bElFUnZZM1Z0Wlc1MElFWnZjbTFoZENJc0ltMXBiV1ZVZVhCbGN5STZXM3NpZEhsd1pTSTZJbUZ3Y0d4cFkyRjBhVzl1TDNCa1ppSXNJbk4xWm1acGVHVnpJam9pY0dSbUluMHNleUowZVhCbElqb2lkR1Y0ZEM5d1pHWWlMQ0p6ZFdabWFYaGxjeUk2SW5Ca1ppSjlYWDBzZXlKdVlXMWxJam9pUTJoeWIyMXBkVzBnVUVSR0lGWnBaWGRsY2lJc0ltUmxjMk55YVhCMGFXOXVJam9pVUc5eWRHRmliR1VnUkc5amRXMWxiblFnUm05eWJXRjBJaXdpYldsdFpWUjVjR1Z6SWpwYmV5SjBlWEJsSWpvaVlYQndiR2xqWVhScGIyNHZjR1JtSWl3aWMzVm1abWw0WlhNaU9pSndaR1lpZlN4N0luUjVjR1VpT2lKMFpYaDBMM0JrWmlJc0luTjFabVpwZUdWeklqb2ljR1JtSW4xZGZTeDdJbTVoYldVaU9pSk5hV055YjNOdlpuUWdSV1JuWlNCUVJFWWdWbWxsZDJWeUlpd2laR1Z6WTNKcGNIUnBiMjRpT2lKUWIzSjBZV0pzWlNCRWIyTjFiV1Z1ZENCR2IzSnRZWFFpTENKdGFXMWxWSGx3WlhNaU9sdDdJblI1Y0dVaU9pSmhjSEJzYVdOaGRHbHZiaTl3WkdZaUxDSnpkV1ptYVhobGN5STZJbkJrWmlKOUxIc2lkSGx3WlNJNkluUmxlSFF2Y0dSbUlpd2ljM1ZtWm1sNFpYTWlPaUp3WkdZaWZWMTlMSHNpYm1GdFpTSTZJbGRsWWt0cGRDQmlkV2xzZEMxcGJpQlFSRVlpTENKa1pYTmpjbWx3ZEdsdmJpSTZJbEJ2Y25SaFlteGxJRVJ2WTNWdFpXNTBJRVp2Y20xaGRDSXNJbTFwYldWVWVYQmxjeUk2VzNzaWRIbHdaU0k2SW1Gd2NHeHBZMkYwYVc5dUwzQmtaaUlzSW5OMVptWnBlR1Z6SWpvaWNHUm1JbjBzZXlKMGVYQmxJam9pZEdWNGRDOXdaR1lpTENKemRXWm1hWGhsY3lJNkluQmtaaUo5WFgxZCxXeUp5ZFMxU1ZTSmQsMCwxLDAsMjQsMjM3NDE1OTMwLDgsMjI3MTI2NTIwLDAsMSwwLC00OTEyNzU1MjMsUjI5dloyeGxJRWx1WXk0Z1RtVjBjMk5oY0dVZ1IyVmphMjhnVjJsdU16SWdOUzR3SUNoWGFXNWtiM2R6SUU1VUlERXdMakE3SUZkcGJqWTBPeUI0TmpRcElFRndjR3hsVjJWaVMybDBMelV6Tnk0ek5pQW9TMGhVVFV3c0lHeHBhMlVnUjJWamEyOHBJRU5vY205dFpTOHhNamd1TUM0d0xqQWdVMkZtWVhKcEx6VXpOeTR6TmlBeU1EQXpNREV3TnlCTmIzcHBiR3hoLGV5SmphSEp2YldVaU9uc2lZWEJ3SWpwN0ltbHpTVzV6ZEdGc2JHVmtJanBtWVd4elpTd2lTVzV6ZEdGc2JGTjBZWFJsSWpwN0lrUkpVMEZDVEVWRUlqb2laR2x6WVdKc1pXUWlMQ0pKVGxOVVFVeE1SVVFpT2lKcGJuTjBZV3hzWldRaUxDSk9UMVJmU1U1VFZFRk1URVZFSWpvaWJtOTBYMmx1YzNSaGJHeGxaQ0o5TENKU2RXNXVhVzVuVTNSaGRHVWlPbnNpUTBGT1RrOVVYMUpWVGlJNkltTmhibTV2ZEY5eWRXNGlMQ0pTUlVGRVdWOVVUMTlTVlU0aU9pSnlaV0ZrZVY5MGIxOXlkVzRpTENKU1ZVNU9TVTVISWpvaWNuVnVibWx1WnlKOWZYMTksNjUsNTIxMDUxOTExLDEsMSwtMSwxNjk5OTU0ODg3LDE2OTk5NTQ4ODcsLTE1NjE0ODE2NzAsNg==',
+    'abt_data': '7.LJPtmpQn5szYPzEEVb84uN44yIpgRwysgfhIWzk2PBpq4FKuHLNub_7pc6v2zeohT78jy48qizli3tL5f3wCr5LjdGwfuCiAsAGiU8iDeAlL3jE2SQ-cNNDvhYC4rj2ZLm9oReuGTS6bF6wvrw0SWZdhsKD3SszassA1udmW0ToCSNgUM6xDyBH-7h3RfwEipkrVmyf1XiGCKQSFGX2CiGC9Wj-kkqE2oK-wIeWIAbxq4MjWv1hTj90hK298gKOWpQSWC4DqT8O4cjzes0lPEqJQD4vLhubQPiq39ouuqWl9vi1oBAmZZ2yokGiMHyV4hVfjF7b2YA9BhFtmLnLiGQ1-RecglshxNR0Cspcob10GoaoIu0op6kvAjkJByE_owCe7YHvaqU9ubvk3-gutTPltlgO7aOamCVKDPyOa7LmKxxFMrFkx_VHtYfIrFi-R0BmI',
+    'is_cookies_accepted': '1',
+}
 
 headers = {
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
@@ -51,6 +63,7 @@ def get_ozon_info(url):
             url,
             params=params,
             headers=headers,
+            cookies=cookies
         ).text
     except:
         time.sleep(60*5)
@@ -58,6 +71,7 @@ def get_ozon_info(url):
             url,
             params=params,
             headers=headers,
+            cookies=cookies
         ).text
 
     soup = BeautifulSoup(txt, 'html.parser')    
@@ -126,6 +140,4 @@ with open(join(dirname, 'chatid.txt'), 'r+') as f:
     else:
         conf.chat_id = int(chatid)
         main()
-    
-
 
